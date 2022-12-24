72c2784592d3f0b15a4aa780cc8ad963122e4366
LucientZ <koblenzpoisiden@gmail.com>
2022-12-24 02:48:28 -0600

    [CHANGE] Changed port to be docker environment variable and 8000 by default

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 
-const port = 8080;
+const port = process.env.PORT || 8000; // Attempts to pull port from docker env variable. If this value is falsy, set default to 8000.
 
 app.set('view engine', 'ejs');
 
+// Get request behavior renders index.ejs as the response
 app.get('/', (req, res) => {
     res.render('pages/index', {});
 });
 
+// Application begins listening on port.
 app.listen(port, () => {
     console.log(`Application is listening on http://localhost:${port}`);
 });
