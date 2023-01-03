bf963884fd090594824340d5205ae0d929ed639a
LucientZ <koblenzpoisiden@gmail.com>
2023-01-02 22:41:06 -0600

    [DOC] Formatted line brakes and added comments

--- src/init.js
+++ src/init.js
@@ -1,25 +1,29 @@
 const fs = require('fs');
 const readline = require('readline');
 
-
-
 function appendEnvVariable(varName, varValue){
+    // Appends a variable to the end of the .env file. Error callback just prints the error.
     fs.appendFile('./config/.env', `${varName}=${varValue}`, (err) =>{
         if(err) console.log(`[ERROR]: ${err}`);
     });
 }
 
 async function promptVarWrite(varName, defaultValue = "", isString = false){
+    // Promise forces code to be blocked so that multiple readline interfaces don't collide
     await new Promise((resolve) =>{
-        const rl = readline.createInterface({input: process.stdin, output: process.stdout});
-        rl.question(`Please enter value for ${varName}: `, (varValue) =>{
+        const rl = readline.createInterface({input: process.stdin, output: process.stdout}); // rl object can be used for command prompt user input
+        rl.question(`Please enter value for ${varName} (default: ${defaultValue}): `, (varValue) =>{
+
+            // If the value inputted is an empty string, assume this means that the default value will be taken
             if(varValue == ""){
                 varValue = defaultValue;
             }
 
+            // If the value of the environment variable is a string, add quotes to variable being written to .env
             if(isString){
                 varValue = `"${varValue}"`;
             }
+
             appendEnvVariable(varName, varValue + '\n');
             rl.close();
             resolve();
@@ -28,12 +32,11 @@ async function promptVarWrite(varName, defaultValue = "", isString = false){
     
 }
 
+// Main is declared and immediately executed.
 (async function main(){
+    // Only prompts for environment variables when ./config/.env doesn't exist
     if(!fs.existsSync('./config/.env')){
         await promptVarWrite('PORT', '8080');
-        await promptVarWrite('DB_URI', '', true);
+        await promptVarWrite('DB_URI', 'none', true);
     }
 })();
-
-
-
