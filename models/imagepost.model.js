96942975d817b66d06ae28472c6fe7fd47e5b3fb
LucientZ <koblenzpoisiden@gmail.com>
2023-01-09 15:34:45 -0600

    [ADD] Added image and imagepost models

---
+++ models/imagepost.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const imagePostSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    attachmentId: {
+        type: String,
+        required: true
+    },
+    attachmentName: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+const ImagePost = mongoose.model('ImagePost', creationSchema);
+module.exports = Creation;
