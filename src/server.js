5b425874de9621fbce2b339a2e7724163b039b34
LucientZ <koblenzpoisiden@gmail.com>
2023-01-10 13:41:16 -0600

    [ADD] Added footer and css formatting. Also created front-end javascript file

--- src/server.js
+++ src/server.js
@@ -6,8 +6,9 @@ app.use(express.urlencoded({extended: true}));
 
 const port = process.env.PORT || 8080; // Attempts to pull port from env variable. If this value is falsy, set default to 8000.
 
-// Set Express to render ejs files
+// Configure Application
 app.set('view engine', 'ejs');
+app.use('/public',express.static('public'))
 
 // Application Routes
 app.get('/', (req, res) => {
@@ -23,8 +24,6 @@ app.get('*', (req, res) =>{
     res.status(404);
     res.render('./pages/404', {title: '404 Not Found'})
 });
-
-
 
 // Application begins listening on port.
 app.listen(port, () => {
