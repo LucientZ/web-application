55e54e4d055a996896014d47afa8733a6b05b6ab
LucientZ <koblenzpoisiden@gmail.com>
2023-01-11 17:07:52 -0600

    [ADD] Added ability to add posts, view posts, view uploaded images, and view a collection of post links.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const mongoose = require('mongoose');
 const Image = require('../models/image.model');
 const ImagePost = require('../models/imagepost.model');
 require('dotenv').config({path: './config/.env'});
@@ -27,30 +28,114 @@ const upload = multer({storage: _storage});
 app.set('view engine', 'ejs');
 app.use('/public',express.static('public'))
 
+// Used to remove mongoose deprecation warning
+mongoose.set('strictQuery', false);
+
 // ========================================================================================
 // Application Routes
 // ========================================================================================
 
 app.get('/', (req, res) => {
-    res.status(200);
-    res.render('./pages/index', {title: 'Main Page'});
+    res.status(200).render('./pages/index', {title: 'Main Page'});
 });
 
-app.get('/create', (req, res) =>{
-    res.render('./pages/create', {title: 'Create Post'});
+app.get('/gallery', (req, res) => {
+    ImagePost.find().sort({createdAt: -1})
+        .then((result) => {
+            res.render('pages/gallery', {posts: result, title: 'Gallery'})
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).render('./pages/error', {title:'Internal Error Occured'});
+        });
+});
+
+app.get('/gallery/post/:postId', (req, res) => {
+    const postId = req.params.postId;
+    ImagePost.findById(postId)
+        .then(result => {
+            res.render('pages/post', {post: result, title: result.title});
+        })
+        .catch((err) => {
+            if(err instanceof mongoose.CastError){
+                res.status(404).redirect('/*');
+            }
+            else{
+                console.log(err);
+                res.status(500).render('./pages/error', {title:'Internal Error Occured'});
+            }
+        });
+});
+
+app.get('/gallery/image/:imageId/:imageName', (req, res) => {
+    const imageId = req.params.imageId;
+    const imageName = req.params.imageName;
+    Image.findById(imageId)
+        .then(result => {
+            if(result.originalname == imageName){
+                res.status(200).contentType(result.mimetype).end(result.buffer["data"]);
+            }
+        })
+        .catch((err) => {
+            console.log(err); 
+            res.status(500).send('Image Couldn\'t be found');
+        });
+});
+
+app.get('/create', (req, res) => {
+    res.status(200).render('./pages/create', {title: 'Create Post'});
 });
 
 app.post('/create', upload.single('attachment'), (req, res) =>{
-    const attachment = req.file;
-    const textArguments = req.body;
+    // Create image object
+    const attachment = new Image({
+        fieldname: req.file.fieldname,
+        originalname: req.file.originalname,
+        mimetype: req.file.mimetype,
+        buffer: {
+            data: req.file.buffer
+        }
+    });
+    
+    attachment.save()
+        .then((result) => {
+            // Create post object
+            const imagePost = new ImagePost({
+                userId: 'PLACEHOLDER_ID', // TODO Add ID validation
+                title: req.body.title,
+                description: req.body.description,
+                tags: ['PLACEHOLDER', 'TAGS'], // TODO Add tag parsing
+                attachmentId: result._id.toString(),
+                attachmentName: req.file.originalname
+            });
+
+            imagePost.save()
+                .then((result) => {
+                    res.redirect('/');
+                })
+                .catch((err) => console.log(err));
+        })
+        .catch((err) => console.log(err));
 });
 
-app.get('*', (req, res) =>{
-    res.status(404);
-    res.render('./pages/404', {title: '404 Not Found'})
+// Route/Page not found handling. Leave this as the last get request
+app.get('/*', (req, res) => {
+    res.status(404).render('./pages/404', {title: '404 Not Found'})
 });
 
-// Application begins listening on port.
-app.listen(PORT, () => {
-    console.log(`Application is listening on ${process.env.HOST_NAME}`);
-});
+// ========================================================================================
+// Server Start
+// ========================================================================================
+
+// Connect to MongoDB
+const connectOptions = {useNewUrlParser: true, useUnifiedTopology: true};
+mongoose.connect(DB_URI, connectOptions)
+    .then((result) => {
+        console.log('Connected to MongoDB database');
+        
+        // Application begins listening on port.
+        app.listen(PORT, () => {
+            console.log(`Application is listening on ${process.env.HOST_NAME}`);
+        });
+    })
+    .catch((err) => console.log(err));
